ba0be337d0fe997a9393f46f61090bcb861f374b
aaron <aaron@pop-os.localdomain>
2021-12-11 23:05:24 -0500

    If file is not included as argument, can run interpreter.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+mod analyzer;
+mod executor;
 mod parser;
+mod symbol_table;
 
 fn main() {
     let mut args = std::env::args();
     let current_program_path = args.next().unwrap();
     let source_path = args.next();
     if source_path.is_none() {
-        eprintln!("{}: Missing argument <file>.calc", current_program_path);
+        //eprintln!("{}: Missing argument <file>.calc", current_program_path);
+        run_interpreter();
     } else {
         process_file(&current_program_path, &source_path.unwrap());
     }
@@ -49,6 +53,67 @@ fn process_file(current_program_path: &str, source_path: &str) {
             return;
         }
     }
+
+    let analyzed_program;
+    let mut variables = symbol_table::SymbolTable::new();
+    match analyzer::analyze_program(&mut variables, &parsed_program) {
+        Ok(analyzed_tree) => {
+            analyzed_program = analyzed_tree;
+        }
+        Err(err) => {
+            eprintln!("Invalid code in '{}': {}", source_path, err);
+            return;
+        }
+    }
     
-    println!("Parsed program: {:#?}", parsed_program);
+    println!("Symbol table: {:#?}", variables);
+    println!("Analyzed program: {:#?}", analyzed_program);
+}
+
+fn run_interpreter() {
+    eprintln!("* Calc interactive interpreter *");
+    let mut variables = symbol_table::SymbolTable::new();
+    loop {
+        let command = input_command();
+        if command.len() == 0 {
+            break;
+        }
+        match command.trim() {
+            "q" => break,
+            "c" => {
+                variables = symbol_table::SymbolTable::new();
+                eprintln!("Cleared variables.");
+            }
+            "v" => {
+                eprintln!("Variables:");
+                for v in variables.iter() {
+                    eprintln!(" {}: {}", v.0, v.1);
+                }
+            }
+            trimmed_command => match parser::parse_program(&trimmed_command) {
+                Ok((rest, parsed_program)) => {
+                    if rest.len() > 0 {
+                        eprintln!("Unparsed input: `{}`.", rest)
+                    } else {
+                        match analyzer::analyze_program(&mut variables, &parsed_program) {
+                            Ok(analyzed_program) => {
+                                executor::execute_program(&mut variables, &analyzed_program)
+                            }
+                            Err(err) => eprintln!("Error: {}", err),
+                        }
+                    }
+                }
+                Err(err) => eprintln!("Error: {:?}", err),
+            },
+        }
+    }
+}
+
+fn input_command() -> String {
+    let mut text = String::new();
+    eprint!("> ");
+    std::io::stdin()
+        .read_line(&mut text)
+        .expect("Cannot read line.");
+    text
 }
